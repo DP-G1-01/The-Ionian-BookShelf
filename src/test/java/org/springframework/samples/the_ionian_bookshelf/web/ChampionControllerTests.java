451f435444ad412742aa190a8c538d47e944c1d9
enrmerver <enrmerver@alum.us.es>
2020-03-26 13:57:50 +0100

    Cambios en champion controller tests

--- src/test/java/org/springframework/samples/the_ionian_bookshelf/web/ChampionControllerTests.java
+++ src/test/java/org/springframework/samples/the_ionian_bookshelf/web/ChampionControllerTests.java
@@ -1,56 +1,55 @@
-//package org.springframework.samples.the_ionian_bookshelf.web;
-//
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.FilterType;
-//import org.springframework.samples.the_ionian_bookshelf.TheIonianBookshelfApplication;
-//import org.springframework.samples.the_ionian_bookshelf.configuration.SecurityConfiguration;
-//import org.springframework.samples.the_ionian_bookshelf.service.ChampionService;
-//import org.springframework.samples.the_ionian_bookshelf.service.ItemService;
-//import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
-//import org.springframework.security.test.context.support.WithMockUser;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//@WebMvcTest(controllers = ChampionController.class,
-//excludeFilters = @ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE
+package org.springframework.samples.the_ionian_bookshelf.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.samples.the_ionian_bookshelf.configuration.SecurityConfiguration;
+import org.springframework.samples.the_ionian_bookshelf.service.ChampionService;
+import org.springframework.samples.the_ionian_bookshelf.service.ItemService;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+//@WebMvcTest(controllers = ChampionController.class
+//,excludeFilters = @ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE
 //, classes = WebSecurityConfigurer.class)
 //, excludeAutoConfiguration = SecurityConfiguration.class)
-//@AutoConfigureMockMvc
-//class ChampionControllerTests {
-//	
-//	@Autowired
-//	private MockMvc mockMvc;
-//	
-//	@MockBean
-//	private ChampionService championService;
-//	
-//	@WithMockUser(value = "spring")
-//	@Test
-//	void testShowChampionListHtml() throws Exception {
-//		mockMvc.perform(get("/champions")).andExpect(status().isOk()).andExpect(model().attributeExists("champions"))
-//		.andExpect(model().attributeExists("role"))
-//				.andExpect(view().name("/champions/listadoCampeones"));
-//	}
-//	
-//	@WithMockUser(value = "spring")
-//	@Test
-//	void testInitCreationForm() throws Exception {
-//		mockMvc.perform(get("/champions/new")).andExpect(status().isOk()).andExpect(model().attributeExists("champion"))
-//				.andExpect(view().name("champions/editCampeon"));
-//	}
+@SpringBootTest
+@AutoConfigureMockMvc
+class ChampionControllerTests {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private ChampionService championService;
+	
+	@WithMockUser(value = "spring")
+	@Test
+	void testShowChampionListHtml() throws Exception {
+		mockMvc.perform(get("/champions")).andExpect(status().isOk()).andExpect(model().attributeExists("champions"))
+		.andExpect(model().attributeExists("role"))
+				.andExpect(view().name("/champions/listadoCampeones"));
+	}
+	
+	@WithMockUser(value = "spring")
+	@Test
+	void testInitCreationFormAnon() throws Exception {
+		mockMvc.perform(get("/champions/new")).andExpect(status().is3xxRedirection());
+	}
 //	@WithMockUser(value = "spring")
 //	@Test
 //	void testProcessCreationFormSuccess() throws Exception {
@@ -58,17 +57,17 @@
 //				.param("health", "900").param("mana", "500").param("energy", "0").param("attack", "1.20").param("speed", "0.90").param("role_id", "1"))
 //				.andExpect(status().is2xxSuccessful());
 //	}
-//	
-//	
-////	@Test
-////    void testProcessCreationFormHasErrors() throws Exception {
-////        mockMvc.perform(post("/champions/new")
-////            .param("name", "Lulu").param("description", "Campeona de League of Legends")
-////			.param("health", "900").param("mana", "500").param("energy", "0").param("attack", "1.0").param("speed", "0.90").param("role_id", "numero"))
-////            .andExpect(model().attributeHasErrors("champion"))
-////            .andExpect(model().attributeHasFieldErrors("champion", "role"))
-////            .andExpect(status().isOk())
-////            .andExpect(view().name("champion/editCampeon"));
-////    }
-//
-//}
+	
+	
+//	@Test
+//    void testProcessCreationFormHasErrors() throws Exception {
+//        mockMvc.perform(post("/champions/new")
+//            .param("name", "Lulu").param("description", "Campeona de League of Legends")
+//			.param("health", "900").param("mana", "500").param("energy", "0").param("attack", "1.0").param("speed", "0.90").param("role_id", "numero"))
+//            .andExpect(model().attributeHasErrors("champion"))
+//            .andExpect(model().attributeHasFieldErrors("champion", "role"))
+//            .andExpect(status().isOk())
+//            .andExpect(view().name("champion/editCampeon"));
+//    }
+
+}
